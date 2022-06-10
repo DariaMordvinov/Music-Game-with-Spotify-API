84ad10d1ea515ecfc972a44b2d536c60e1f12b94
Dasha <daria.sidelnikova.2020@gmail.com>
2022-06-10 15:41:16 -0400

    make it better

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from .helpers import make_options, make_artists, get_track
+from helpers import make_options, make_artists, get_track
 
 app = Flask(__name__)
 
